851d5a2e8555eb578e8000f0393477883e971eaf
LeonardoNunes-N <leonardonnegrao@outlook.com>
2025-02-27 14:36:57 -0300

    Ajuste de codigo para receber ip estatico da master

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i "s/SPARK_MASTER_IP_PLACEHOLDER/${SPARK_MASTER_IP}/g" $SPARK_HOME/conf/spark-defaults.conf
+
 SPARK_WORKLOAD=$1
 echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
 if [ "$SPARK_WORKLOAD" == "master" ];
@@ -8,7 +10,7 @@ then
   start-master.sh -p 7077 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
-  start-worker.sh spark://spark-master:7077
+  start-worker.sh spark://${SPARK_MASTER_IP}:7077
 elif [ "$SPARK_WORKLOAD" == "history" ]
 then
   start-history-server.sh
